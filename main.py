742a297c575b76b70d6bd4b4aeaa7390174d3410
Paul Ambrose <pambrose@mac.com>
2020-02-17 21:15:38 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    light.show_ring("red blue red yellow red red red red red red")
+
+forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,3 @@
+forever(function on_forever() {
+    light.showRing("red blue red yellow red red red red red red", 400)
+})
